d9ee3667d2e1a9650219084921ce192a929d4127
Wajahat Riaz <wajahat.riaz@10xengineers.com>
2024-06-26 03:40:40 +0500

    Adding a tcl for gtkwave

---
+++ siglist_wave.tcl
@@ -0,0 +1,56 @@
+gtkwave::addSignalsFromList rvtop.clk
+
+gtkwave::addSignalsFromList  rvtop.veer.dec.ifu.ifc.ifc_fetch_addr_f1_ext
+gtkwave::addSignalsFromList  rvtop.veer.dec.ifu.ifu_fetch_data
+
+gtkwave::addSignalsFromList  rvtop.veer.dec.ifu.ifu_i0_instr
+gtkwave::addSignalsFromList  rvtop.veer.dec.ifu.ifu_i1_instr
+
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.dec_i0_instr_d
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.dec_i1_instr_d
+
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.raddr0
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.raddr1
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.raddr2
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.raddr3
+
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.rd0
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.rd1
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.rd2
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.rd3
+
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i0_inst_e1
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i1_inst_e1
+
+gtkwave::addSignalsFromList rvtop.veer.exu.mul_e1.a_e1
+gtkwave::addSignalsFromList rvtop.veer.exu.mul_e1.b_e1
+
+gtkwave::addSignalsFromList rvtop.veer.exu.i1_alu_e1.a_ff
+gtkwave::addSignalsFromList rvtop.veer.exu.i1_alu_e1.b_ff
+
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i0_inst_e2
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i1_inst_e2
+
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i0_inst_e3
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i1_inst_e3
+
+gtkwave::addSignalsFromList rvtop.veer.exu.exu_mul_result_e3
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i1_result_e3
+
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i0_inst_e4
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i1_inst_e4
+
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i0_result_e4
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i1_result_e4
+
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i0_inst_wb
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i1_inst_wb
+
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i0_result_wb
+gtkwave::addSignalsFromList rvtop.veer.dec.decode.i1_result_wb
+
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.waddr0
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.waddr1
+
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.wd0
+gtkwave::addSignalsFromList rvtop.veer.dec.arf.wd1
